774cc76eef7cdff90ed1654dc8cc32812f928d11
Klaysb <sclbrito@hotmail.com>
2022-12-08 21:10:37 +0000

    feat: add day4

---
+++ AdventOfCode/Day4/Day4.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.Day4;
+
+public class Day4
+{
+    public static void Run_Day4()
+    {
+        var assignments = File.ReadAllLines("Day4/input.txt");
+
+        var assignmentPairs = AssignmentPairs(assignments);
+        var assignmentPairsOverlap = AssignmentPairsOverlap(assignments);
+
+        Console.WriteLine("Day 4:");
+        Console.WriteLine($"\tAssignment pairs: {assignmentPairs}");
+        Console.WriteLine($"\tAssignment pairs overlap: {assignmentPairsOverlap}");
+        Console.WriteLine();
+    }
+
+    public static int AssignmentPairs(IEnumerable<string> assignments)
+    {
+        return assignments
+            .Select(assignment => assignment
+                .Split(',') // { "2-4", "6-8" }
+                .Select(pair => pair.Split('-').Select(int.Parse).ToArray()) // { [2,4], [6,8] }
+                .Select(pair => (pair[0], pair[1])) // { (2,4), (6,8) }
+                .ToArray()) // [ (2,4), (6,8) ]
+            .Count(pairs => 
+                pairs[0].Item1 >= pairs[1].Item1 && pairs[0].Item2 <= pairs[1].Item2 || 
+                pairs[1].Item1 >= pairs[0].Item1 && pairs[1].Item2 <= pairs[0].Item2);
+    }
+    
+    public static int AssignmentPairsOverlap(IEnumerable<string> assignments)
+    {
+        return assignments
+            .Select(assignment => assignment
+                .Split(',') // { "2-4", "6-8" }
+                .Select(pair => pair.Split('-').Select(int.Parse).ToArray()) // { [2,4], [6,8] }
+                .Select(pair => (pair[0], pair[1])) // { (2,4), (6,8) }
+                .ToArray()) // [ (2,4), (6,8) ]
+            .Count(pairs => 
+                pairs[0].Item2 >= pairs[1].Item1 && pairs[0].Item2 <= pairs[1].Item2 ||
+                pairs[1].Item2 >= pairs[0].Item1 && pairs[1].Item2 <= pairs[0].Item2);
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,6 +1,7 @@
 ﻿using static AdventOfCode.Day1.Day1;
 using static AdventOfCode.Day2.Day2;
 using static AdventOfCode.Day3.Day3;
+using static AdventOfCode.Day4.Day4;
 
 namespace AdventOfCode;
 
@@ -11,5 +12,6 @@ public class Program
         Run_Day1();
         Run_Day2();
         Run_Day3();
+        Run_Day4();
     }
 }
